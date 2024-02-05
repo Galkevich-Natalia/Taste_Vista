3dca367aa7c435e7e5ebbab478c50361b635011c
Galkevich-Natalia <galkevich.natalia@gmail.com>
2024-02-05 15:28:55 +0300

    displayed the cards

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,10 +1,13 @@
 import { getMenu } from "../../api/getApi";
+import { addMenu } from "./createCards";
 
 async function getCardsData() {
     try {
       const data = await getMenu();
-      console.log(data);
-    //   data.forEach((obj) => );
+      console.log(data)
+    //   console.log(data[0].drinks);
+    const arr = data[0].drinks;
+    arr.forEach((obj) => addMenu(obj));
     } catch (e) {
       console.error("error", e);
       throw e;
---
+++ src/components/card/createCards.js
@@ -0,0 +1,127 @@
+const cardsContainer = document.querySelector('.cards__container');
+
+export function addMenu(data) {
+    cardsContainer.append(crtCard(data));
+}
+
+function crtCard(data) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.append(crtCardContainer(data));
+
+    return card;
+}
+
+function crtCardContainer(data) {
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add('card__container');
+    cardContainer.append(crtCardWrapper(data));
+
+    return cardContainer;
+}
+
+function crtCardWrapper(data) {
+    const cardWrapper = document.createElement('div');
+    cardWrapper.classList.add('card__wrapper');
+    cardWrapper.append(crtCardContImage(), crtCardContent(data));
+
+    return cardWrapper;
+}
+
+function crtCardContImage() {
+    const cardContImage = document.createElement('div');
+    cardContImage.classList.add('card__image');
+    // cardContImage.append(crtCardImg());
+
+    return cardContImage;
+}
+
+// function crtCardImg() {
+//     const cardImg = document.createElement('img');
+//     cardImg.classList.add('card__img');
+    
+//     return cardImg;
+// }
+
+function crtCardContent(data) {
+    const cardContent = document.createElement('div');
+    cardContent.classList.add('card__content');
+    cardContent.append(crtCardName(data), crtCardIngredients(), crtCardFooter(data));
+
+    return cardContent;
+}
+
+function crtCardName(data) {
+    const container = document.createElement('div');
+    container.classList.add('card__name');
+    container.append(crtTextName(data), crtTextSize(data));
+
+    return container;
+}
+
+function crtTextName(value) {
+    const textName = document.createElement('span');
+    textName.classList.add('card__text');
+    textName.textContent = value.name;
+
+    return textName;
+}
+
+function crtTextSize(value) {
+    const textSize = document.createElement('span');
+    textSize.classList.add('card__size');
+    textSize.textContent = value.size;
+
+    return textSize;
+}
+
+function crtCardIngredients() {
+    const cardIngredients = document.createElement('div');
+    cardIngredients.classList.add('card__ingredients');
+    
+    const cardIngredientsText = document.createElement('p');
+
+    cardIngredients.append(cardIngredientsText);
+
+    return cardIngredients;
+}
+
+function crtCardFooter(data) {
+    const cardFooter = document.createElement('div');
+    cardFooter.classList.add('card__footer');
+    cardFooter.append(crtCardContainerCost(data), crtCardConteinerBtn());
+
+    return cardFooter;
+}
+
+function crtCardContainerCost(data) {
+    const cardContainerCost = document.createElement('div');
+    cardContainerCost.classList.add('card__cost');
+    cardContainerCost.append(crtCardCost(data));
+
+    return cardContainerCost;
+}
+
+function crtCardCost(value) {
+    const cardCost = document.createElement('p');
+    cardCost.classList.add('card__cost-price');
+    cardCost.textContent = value.price;
+   
+   return cardCost;
+}
+
+function crtCardConteinerBtn() {
+    const cardContainerBtn = document.createElement('div');
+    cardContainerBtn.classList.add('card__btn');
+    cardContainerBtn.append(crtCardBtn());
+
+    return cardContainerBtn;
+}
+
+function crtCardBtn() {
+    const cardBtn = document.createElement('button');
+    cardBtn.classList.add('button');
+    cardBtn.textContent = '+';
+
+    return cardBtn;
+}
