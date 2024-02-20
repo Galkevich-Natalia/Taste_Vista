93451449b68c91fc0882775d7ae830f5acfc5e08
Galkevich-Natalia <galkevich.natalia@gmail.com>
2024-02-20 23:59:21 +0300

    added styling of the selected category buttons

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -24,17 +24,38 @@ function deleteCardsByCategory() {
   cards.forEach(item => item.remove());
 }
 
+function deleteStylesBtnsCategories() {
+  const allBtnsMenu = document.querySelectorAll(".menu__item-button");
+  allBtnsMenu.forEach(item => item.classList.remove("menu__item-button_active"));
+}
+
 function getMenuByCategory() {
-  const itemMenu = document.querySelectorAll(".menu__item button");
+  const itemMenu = document.querySelectorAll(".menu__item-button");
   Array.from(itemMenu).forEach(item => item.addEventListener("click", selectedCategory));
 }
 
 function selectedCategory(event) {
+  
+  deleteStylesBtnsCategories();
+
+  const btnCategory = event.target;
+  // console.log(event);
+
+  btnCategory.classList.add("menu__item-button_active");
+
   const dataCategory = event.target.textContent;
+  
   getCardsData(dataCategory);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+
+  const navBtnsCategories = document.querySelectorAll(".menu__item-button");
+
+  const defaultCategoryBtn = navBtnsCategories[0];
+
+  defaultCategoryBtn.classList.add("menu__item-button_active");
+
   getCardsData("snacks");
   getMenuByCategory();
 });
