d12cc99fbcd3e1aacf673e1945ca172b31205e84
Galkevich-Natalia <galkevich.natalia@gmail.com>
2024-04-26 21:42:05 +0300

    added fn checkValidation, fixed fn validateForm

--- src/components/validation/validation.js
+++ src/components/validation/validation.js
@@ -1,10 +1,13 @@
+import {
+    REGNAME, REGPHONE, REGEMAIL,
+    REGCITY, REGSTREET, REGHOUSE, REGAPARTMENT
+} from "../../constants/constants";
+
 function validateName() {
     const name = document.getElementById('name').value;
     const messageName = document.getElementById('messageName');
 
-    const re = /^[A-Za-zА-Яа-яЁё]{6,20}(?:[-\s'][A-Za-zА-Яа-яЁё]{1,19})?$/;
-
-    if (!re.test(name)) {
+    if (!REGNAME.test(name)) {
         messageName.textContent = 'Проверьте данные Name';
         messageName.style.color = "red";
         return false;
@@ -18,9 +21,7 @@ function validatePhone() {
     const phone = document.getElementById('phone').value;
     const messagePhone = document.getElementById('messagePhone');
 
-    const re = /^\+375\d{9}$/;
-
-    if (!re.test(phone)) {
+    if (!REGPHONE.test(phone)) {
         messagePhone.textContent = 'Проверьте данные Phone';
         messagePhone.style.color = "red";
         return false;
@@ -34,9 +35,7 @@ function validateEmail() {
     const email = document.getElementById('email').value;
     const messageEmail = document.getElementById('messageEmail');
 
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (!re.test(email)) {
+    if (!REGEMAIL.test(email)) {
         messageEmail.textContent = 'Проверьте данные Email';
         messageEmail.style.color = "red";
         return false;
@@ -50,9 +49,7 @@ function validateCity() {
     const city = document.getElementById('city').value;
     const messageCity = document.getElementById('messageCity');
 
-    const re = /^[a-zA-Z\-]{3,20}$/;
-
-    if (!re.test(city)) {
+    if (!REGCITY.test(city)) {
         messageCity.textContent = 'Проверьте данные City';
         messageCity.style.color = "red";
         return false;
@@ -66,9 +63,7 @@ function validateStreet() {
     const street = document.getElementById('street').value;
     const messageStreet = document.getElementById('messageStreet');
 
-    const re = /^[a-zA-Z\s\-]+$/;
-
-    if (!re.test(street)) {
+    if (!REGSTREET.test(street)) {
         messageStreet.textContent = 'Проверьте данные Street';
         messageStreet.style.color = "red";
         return false;
@@ -82,9 +77,7 @@ function validateHouse() {
     const house = document.getElementById('house').value;
     const messageHouse = document.getElementById('messageHouse');
 
-    const re = /^\d+[A-Za-z]?(\/\d+)?$/;
-
-    if (!re.test(house)) {
+    if (!REGHOUSE.test(house)) {
         messageHouse.textContent = 'Проверьте данные House';
         messageHouse.style.color = "red";
         return false;
@@ -98,9 +91,7 @@ function validateApartment() {
     const apartment = document.getElementById('apartment').value;
     const messageApartment = document.getElementById('messageApartment');
 
-    const re = /^\d+[A-Za-z]?(\/\d+)?$/;
-
-    if (!re.test(apartment)) {
+    if (!REGAPARTMENT.test(apartment)) {
         messageApartment.textContent = 'Проверьте данные Apartment';
         messageApartment.style.color = "red";
         return false;
@@ -110,25 +101,30 @@ function validateApartment() {
     }
 }
 
-export function validateForm() {
-    const checkoutModal = document.querySelector('.checkout__modal');
-    const overlayCheckout = document.querySelector('.overlayCheckout');
-    const body = document.querySelector('body');
+function validateForm() {
     const arrayCalls = [validateName(), validatePhone(), validateEmail(), validateCity(), validateStreet(), validateHouse(), validateApartment()];
+    const isValidForm = arrayCalls.every((item) => item === true);
 
-    const isValidResult = arrayCalls.every((item) => item === true)
+    return isValidForm;
+}
 
-    if (isValidResult) {
+export function checkValidation() {
+    if (validateForm()) {
+        const checkoutModal = document.querySelector('.checkout__modal');
+        const overlayCheckout = document.querySelector('.overlayCheckout');
+        const body = document.querySelector('body');
+
         checkoutModal.style.display = 'block';
         overlayCheckout.style.display = 'block';
         body.classList.add('checkout__modal-open');
     }
 }
 
+
 const checkoutModalBtnClose = document.querySelector('.checkout__modal-btn');
 checkoutModalBtnClose.addEventListener('click', closeCheckoutModal);
 
-function closeCheckoutModal(event) {
+function closeCheckoutModal() {
     const checkoutModal = document.querySelector('.checkout__modal');
     const overlayCheckout = document.querySelector('.overlayCheckout');
     const body = document.querySelector('body');
