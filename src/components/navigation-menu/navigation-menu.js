28d51ba340daf85bfa08949864009aad9c693c5d
Galkevich-Natalia <galkevich.natalia@gmail.com>
2024-05-22 23:44:36 +0300

    added burger-menu & side-bar

---
+++ src/components/navigation-menu/navigation-menu.js
@@ -0,0 +1,23 @@
+const burgerMenu = document.querySelector('.burger-menu');
+const sideBarCross = document.querySelector('.side-bar__cross');
+const sideBar = document.querySelector('.side-bar');
+const overlaySideBar = document.querySelector('.overlaySideBar');
+const body = document.querySelector('body');
+
+burgerMenu.addEventListener('click', openSideBar);
+sideBarCross.addEventListener('click', closeSideBar);
+
+function openSideBar() {
+    burgerMenu.style.display = 'none';
+    sideBar.style.display = 'block';
+    overlaySideBar.style.display = 'block';
+    overlaySideBar.addEventListener('click', closeSideBar);
+    body.classList.add('hideScroll');
+}
+
+function closeSideBar() {
+    sideBar.style.display = 'none';
+    burgerMenu.style.display = 'block';
+    overlaySideBar.style.display = 'none';
+    body.classList.remove('hideScroll');
+}
